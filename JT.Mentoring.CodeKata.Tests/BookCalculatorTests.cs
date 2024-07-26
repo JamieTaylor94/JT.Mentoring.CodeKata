ca1ec99f0397298648a9bbf0fb17f6b874afd0f7
Jamie Taylor <jamie.taylor@moonpig.com>
2024-07-26 15:21:12 +0100

    initial commit

---
+++ JT.Mentoring.CodeKata.Tests/BookCalculatorTests.cs
@@ -0,0 +1,92 @@
+﻿using Xunit;
+namespace JT.Mentoring.CodeKata.Tests;
+
+public class BookCalculatorTests
+{
+    private readonly BookCalculator _bookCalculator;
+
+    public BookCalculatorTests()
+    {
+        _bookCalculator = new BookCalculator();
+    }
+
+    [Fact]
+    public void No_Books_Cost_Nothing()
+    {
+        var result = _bookCalculator.Calculate([]);
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void One_Book_Costs_EightEuros()
+    {
+        var result = _bookCalculator.Calculate(["one"]);
+
+        Assert.Equal(8, result);
+    }
+
+    [Fact]
+    public void Two_Identical_Books_Cost_SixteenEuros()
+    {
+        var result = _bookCalculator.Calculate(["one", "one"]);
+
+        Assert.Equal(16, result);
+    }
+
+    [Fact]
+    public void Two_Different_Books_Have_Five_Percent_Discount()
+    {
+        var result = _bookCalculator.Calculate(["one", "two"]);
+
+        Assert.Equal(15.2m, result);
+    }
+
+    [Fact]
+    public void Three_Different_Books_Ten_Percent_Discount()
+    {
+        var result = _bookCalculator.Calculate(["one", "two", "three"]);
+
+        Assert.Equal(21.6m, result);
+    }
+
+    [Fact]
+    public void Four_Different_Books_Twenty_Percent_Discount()
+    {
+        var result = _bookCalculator.Calculate(["one", "two", "three", "four"]);
+
+        Assert.Equal(25.6m, result);
+    }
+
+    [Fact]
+    public void Five_Different_Books_Twenty_Five_Percent_Discount()
+    {
+        var result = _bookCalculator.Calculate(["one", "two", "three", "four", "five"]);
+
+        Assert.Equal(30, result);
+    }
+
+    [Fact]
+    public void Two_Different_Books_Five_Percent_Discount_One_Is_FullPrice()
+    {
+        var result = _bookCalculator.Calculate(["one", "one", "two"]);
+
+        Assert.Equal(23.2m, result);
+    }
+
+    [Fact]
+    public void Two_Sets_Have_Ten_Percent_Discount_Each()
+    {
+        var result = _bookCalculator.Calculate(["one", "one", "two", "two"]);
+
+        Assert.Equal(30.4m, result);
+    }
+
+    [Fact]
+    public void The_Answer()
+    {
+        var result = _bookCalculator.Calculate(["one", "one", "two", "two", "three", "three", "four", "five"]);
+
+        Assert.Equal(51.60m, result);
+    }
+}
---
+++ JT.Mentoring.CodeKata/BookCalculator.cs
@@ -0,0 +1,48 @@
+﻿namespace JT.Mentoring.CodeKata;
+
+public class BookCalculator
+{
+    private readonly int SingleBookPrice = 8;
+
+    private Dictionary<int, decimal> _discounts = new() {
+        { 0, 0 },
+        { 1, 1 },
+        { 2, 0.95m },
+        { 3, 0.9m },
+        { 4, 0.8m },
+        { 5, 0.75m}
+    };
+
+    public decimal Calculate(string[] books)
+    {
+        var basketCollection = new List<List<string>>();
+        decimal totalPrice = 0;
+
+        foreach (var book in books)
+        {
+            bool isBookAdded = false;
+
+            foreach (var basket in basketCollection)
+            {
+                if (!basket.Contains(book))
+                {
+                    basket.Add(book);
+                    isBookAdded = true;
+                    break;
+                }
+            }
+
+            if (!isBookAdded)
+            {
+                basketCollection.Add([book]);
+            }
+        }
+
+        foreach (var basket in basketCollection)
+        {
+            totalPrice += basket.Count * SingleBookPrice * _discounts[basket.Count];
+        }
+
+        return totalPrice;
+    }
+}
